b6da1cbcb37ad13f5e60895cca52ca37f5113f43
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-16 01:41:14 +0700

    migration interface di-container in di-container.ts

--- src/utils/di-container.ts
+++ src/utils/di-container.ts
@@ -1,4 +1,23 @@
-import { IDIContainer } from '@/types/utils/di-container';
+// DI-контайнер - интструмент для управления зависимостями в приложении
+// в данном случае он самописный, но в дальнейшем можно использовать готовые реализации
+
+export interface IDIContainer {
+  /**
+   * регистрирует сервис в контейнере
+   * @param key - Уникальный ключ сервиса (например, имя класса или интерфейса).
+   * @param instance - Экземпляр сервиса.
+   */
+  register<T>(key: string, instance: T): void;
+  
+  /**
+   * Получает сервис из контейнера.
+   * @param key - Уникальный ключ сервиса.
+   * @returns Экземпляр сервиса.
+   * @throws Ошибка, если сервис не найден.
+   */
+  get<T>(key: string): T;
+}
+
 
 export class DIContainer implements IDIContainer {
   private services: Map<string, any> = new Map();
