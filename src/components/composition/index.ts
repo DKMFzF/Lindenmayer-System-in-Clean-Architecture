f2a30f0a83b593ec47240ba475d955e93928876c
DKMFzF <150276575+DKMFzF@users.noreply.github.com>
2025-02-16 02:33:45 +0700

    add geometry global composition in global core

--- src/components/composition/index.ts
+++ src/components/composition/index.ts
@@ -1,4 +1,6 @@
 import { container } from '@/components/composition/core';
 import '@/components/l-system/composition';
+import '@/components/geometry/location/composition'
+import '@/components/geometry/shape/composition'
 
 export { container }
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import './scss/styles.scss'
 import { container } from '@/components/composition';
 import { ISystemBuilder } from '@/components/l-system/types';
+import { IStartSelector } from "@/components/geometry/location/types";
+import { IShapeBuilder } from "@/components/geometry/shape/types";
 
-const builder = container.get<ISystemBuilder>('ISystemBuilder');
-const buildValue =  builder.build({
-  initiator: '0',
-  iterations: 3,
-  rules: { '1': '11', '0': '1[0]0' }
-})
+// Testing ISystemBuilder
+// const builder = container.get<ISystemBuilder>('ISystemBuilder');
+// const buildValue =  builder.build({
+//   initiator: '0',
+//   iterations: 3,
+//   rules: { '1': '11', '0': '1[0]0' }
+// });
+// console.log(buildValue);
 
-console.log(buildValue);
+// Testin IStartSelector
+// const geimetryLocation = container.get<IStartSelector>('IStartSelector');
+// console.log(geimetryLocation.selectStart());
+
+// Testing 
+// const geimetryShape = container.get<IShapeBuilder>('IShapeBuilder');
+// console.log(geimetryShape.createLine({ x: 0, y: 20 }, 20));
